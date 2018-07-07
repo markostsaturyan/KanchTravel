fca1ffa09caff63e2423c7d1a07f49c9e0d09bf2
AnjelaB <anjelabadalyan96@gmail.com>
2018-07-07 19:31:15 +0400

    Test quary changes

--- Users/Queries.sql
+++ Users/Queries.sql
@@ -1,6 +1,13 @@
-﻿Insert into UsersDB.dbo.Roles (RoleName)
+﻿Select * from Roles;
+
+Insert into UsersDB.dbo.Users()
 Values('Admin'),
 ('User'),
 ('Driver'),
 ('Guide'),
 ('Photographer');
+
+
+
+Insert into Users (FirstName, LastName, Sex, DateOfBirth, PhoneNumber, Email, UserName, [Password], IsActive)
+Values('Anjela', 'Badalyan', 1, '19960830', '+37498889928', 'anjelabadalyan96@gmail.com','anjela','anjela', 1 )
