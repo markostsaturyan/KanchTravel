c94c0a56ea2bf294cc8ded11dd62eed7bb004065
AnjelaB <anjelabadalyan96@gmail.com>
2018-07-08 20:20:22 +0400

    Added stored procedures

--- Users/GetDrivers.sql
+++ Users/GetDrivers.sql
@@ -1,19 +1,8 @@
-﻿Select * from Roles;
-
-Insert into UsersDB.dbo.Users()
-Values('Admin'),
-('User'),
-('Driver'),
-('Guide'),
-('Photographer');
-
-
-
-Insert into Users (FirstName, LastName, Sex, DateOfBirth, PhoneNumber, Email, UserName, [Password], IsActive)
-Values('Anjela', 'Badalyan', 1, '19960830', '+37498889928', 'anjelabadalyan96@gmail.com','anjela','anjela', 1 )
-
+﻿CREATE PROCEDURE [dbo].[GetDrivers]
+AS
 	SELECT UserId, FirstName, LastName, DateOfBirth, Email, PhoneNumber, Picture, UserName, [Password], IsActive, 
 	DrivingLicencePicBack, DrivingLicencePicFront, KnowledgeOfLanguages, Rating, 
 	Car.Id as CarId, Car.Brand as Brand, CarPicture1, CarPicture2, CarPicture3, FuelType, Car.HasAirConditioner, Car.HasKitchen, Car.HasMicrophone, Car.HasToilet
 	FROM Users JOIN Drivers ON Users.Id = Drivers.UserId
 	Join Car On Drivers.CarId=Car.Id
+RETURN 0
