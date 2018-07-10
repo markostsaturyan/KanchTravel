2d500ee781fc91c8d1ff1036bbed2aff2b57eec6
AnjelaB <anjelabadalyan96@gmail.com>
2018-07-10 19:07:08 +0400

    Any changes in UserDB

---
+++ Users/InsertUser.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[InsertUser]
+	@FirstName nvarchar(Max),
+	@LastName nvarchar(Max),
+	@Gender bit,
+	@DateOfBirth Date,
+	@PhonNumber  nvarchar(Max),
+	@Email nvarchar(Max),
+	@UserName nvarchar(Max),
+	@Password nvarchar(Max)
+AS
+	Insert into Users (FirstName, LastName, Gender, DateOfBirth, PhoneNumber, Email, UserName, [Password], IsActive)
+	Values(@FirstName, @LastName, @Gender, @DateOfBirth, @PhonNumber, @Email,@UserName,@Password, 1)
+
+	Insert into UserRole Values((Select SCOPE_IDENTITY()),(Select Id from Roles Where RoleName='User'));
+RETURN 0
--- Users/Queries.sql
+++ Users/Queries.sql
@@ -7,9 +7,12 @@ Values('Admin'),
 ('Guide'),
 ('Photographer');
 
+Delete FRom Users where Users.Id=6
 
 
-Insert into Users (FirstName, LastName, Sex, DateOfBirth, PhoneNumber, Email, UserName, [Password], IsActive)
+Select * from Users
+
+Insert into Users (FirstName, LastName, Gender, DateOfBirth, PhoneNumber, Email, UserName, [Password], IsActive)
 Values('Anjela', 'Badalyan', 1, '19960830', '+37498889928', 'anjelabadalyan96@gmail.com','anjela','anjela', 1 )
 
 	SELECT UserId, FirstName, LastName, DateOfBirth, Email, PhoneNumber, Picture, UserName, [Password], IsActive, 
@@ -17,3 +20,9 @@ Values('Anjela', 'Badalyan', 1, '19960830', '+37498889928', 'anjelabadalyan96@gm
 	Car.Id as CarId, Car.Brand as Brand, CarPicture1, CarPicture2, CarPicture3, FuelType, Car.HasAirConditioner, Car.HasKitchen, Car.HasMicrophone, Car.HasToilet
 	FROM Users JOIN Drivers ON Users.Id = Drivers.UserId
 	Join Car On Drivers.CarId=Car.Id
+
+	SELECT Users.Id, FirstName, LastName, UserName, [Password], Email, Roles.RoleName, IsActive
+	FROM Users join UserRole on Users.Id=UserId
+		 join Roles on RoleId=Roles.Id
+
+		
